d12dd7a366e75299763e7fe2f5872318f9b8d3c9
radhika0406 <radhika.shah@peak.ai>
2022-11-27 18:18:31 +0530

    feat; add a calculator app

---
+++ calculator/calculator.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
+  req.body.num1;
+  res.send("The result of calculation is " + result);
+});
+
+app.listen(3000, function () {
+  console.log("Server is running at Port 3000");
+});
